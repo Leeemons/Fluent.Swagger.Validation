44df0548ed1e1fbc8ab9d39b6270f7f76b29974e
Ladislav Schumacher <ladislav.schumacher4@gmail.com>
2020-10-10 15:10:22 +0200

    add custom types to DI

--- Fluent.Swagger.Validation/ServiceCollectionExtension.cs
+++ Fluent.Swagger.Validation/ServiceCollectionExtension.cs
@@ -1,14 +1,20 @@
 ﻿using Fluent.Swagger.Validation.Resolvers;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Fluent.Swagger.Validation
 {
     public static class ServiceCollectionExtension
     {
-        public static void AddFluentSwaggerValidation(this IServiceCollection services)
+        public static void AddFluentSwaggerValidation(this IServiceCollection services, params Type[] types)
         {
+            var typesList = types.ToList();
+            typesList.Add(typeof(ServiceCollectionExtension));
+
             services.Scan(a =>
-                a.FromAssembliesOf(typeof(ServiceCollectionExtension))
+                a.FromAssembliesOf(typesList)
                 .AddClasses(c => c.AssignableTo<IResolver>())
                 .AsImplementedInterfaces()
                 .WithSingletonLifetime());
